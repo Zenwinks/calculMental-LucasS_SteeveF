1567afbaf76b54a02dbae0d29247946ca5a77891
sfondain <steeve.fondain@gmail.com>
2019-10-20 18:36:49 +0200

    UserDAOJDBC best_score Update

--- src/dal/UserDAOJDBC.java
+++ src/dal/UserDAOJDBC.java
@@ -11,6 +11,7 @@ public class UserDAOJDBC extends DataAccessObjectJDBC<User> {
     private static final String AUTHENT_QUERY = "SELECT * FROM user WHERE login = ? AND password = ?";
     private static final String GET_LEADERBOARD = "SELECT login,best_score FROM user ORDER BY best_score DESC LIMIT 10";
     private static final String NEW_USER_QUERY = "INSERT INTO user VALUES (null,?,?,0,0)";
+    private static final String UPDATE_BEST_SCORE = "UPDATE user SET best_score = ? WHERE id=?";
 
     public UserDAOJDBC(String dbUrl, String dbLogin, String dbPwd) {
         super(dbUrl, dbLogin, dbPwd);
@@ -22,6 +23,15 @@ public class UserDAOJDBC extends DataAccessObjectJDBC<User> {
              PreparedStatement ps = connection.prepareStatement(NEW_USER_QUERY)) {
             ps.setString(1,newUser.getLogin());
             ps.setString(2,newUser.getPassword());
+            ps.executeUpdate();
+        }
+    }
+
+    public void updateBestScore(int bestScore, int idUser) throws SQLException{
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbLogin, dbPwd);
+             PreparedStatement ps = connection.prepareStatement(UPDATE_BEST_SCORE)) {
+            ps.setInt(1,bestScore);
+            ps.setInt(2,idUser);
             ps.executeUpdate();
         }
     }
